c420f24a532a1ccaea2beec4bf7b9ec6ffcbc345
kimberlyamaya <kimberly.kimbell89@gmail.com>
2022-03-05 09:00:05 -0800

    started adding mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,39 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
-  type Query { 
-    users: [User]
-    user(username: String!): User
-  }
 
   type User {
     _id: ID
     username: String
     email: String
+    bookCount: Int
     savedBooks: [Book]
   }
 
   type Book {
-    authors: String
+    bookId: ID
+    authors: [String]
     description: String
-    bookId: String
+    title: String 
     image: String
     link: String
-    title: String   
   }
 
+  type Query { 
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation { 
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 ` 
 
 module.exports = typeDefs
